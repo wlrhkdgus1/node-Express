c490e29f099c49af337bd1461020f90ebe96e863
Gwang Hyun <gwgw4578@gmail.com>
2020-10-01 11:32:29 +0900

    1001 passport

--- main.js
+++ main.js
@@ -19,6 +19,61 @@ app.use(session({
   store:new FileStore()
 }))
 
+var authData = {
+  email: 'amarans@gmail.com',
+  password: '111111',
+  nickname: 'amarans'
+}
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.serializeUser(function(user, done) {
+    console.log('serializeUser', user);  //로그인에 성공 했을때 딱 1번
+    done(null, user.email);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    console.log('deserializeUser', id); // 그데이터를 기준으로 돌아다닐떄마다.
+    done(null, authData);
+  });
+
+  passport.use(new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'pwd'     
+    },
+    function(username, password, done) {
+      console.log('LocalStrategy', username, password);
+      if(username === authData.email){
+        console.log(1);
+        if(password === authData.password){
+          console.log(2);
+          return done(null, authData);
+        } else {
+          console.log(3);
+          return done(null, false, {
+            message: 'Incorrect password.'
+          });
+        }
+      } else {
+        console.log(4);
+        return done(null, false, {
+          message: 'Incorrect username.'
+        });
+      }
+    }
+  ));
+       
+  app.post('/auth/login_process',
+  passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/auth/login' 
+  }));
+
 app.get('*', function(request, response, next){
   fs.readdir('./data', function(error, filelist){
     request.list = filelist;
