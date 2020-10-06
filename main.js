4628bd4bffcebad686a249e4944454b84e0f4ddf
Gwang Hyun <gwgw4578@gmail.com>
2020-10-06 20:25:10 +0900

    1006 flash study

--- main.js
+++ main.js
@@ -7,75 +7,25 @@ var helmet = require('helmet')
 app.use(helmet());
 var session = require('express-session')
 var FileStore = require('session-file-store')(session)
-
+var flash = require('connect-flash');
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(compression());
 app.use(session({
   secret: 'asadlfkj!@#!@#dfgasdg',
   resave: false,
   saveUninitialized: true,
-  store:new FileStore()
+  //store: new FileStore()
 }))
+app.use(flash());
 
-var authData = {
-  email: 'amarans@gmail.com',
-  password: '111111',
-  nickname: 'amarans'
-}
+var passport = require('./lib/passport')(app);
 
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-  
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  passport.serializeUser(function(user, done) {
-    console.log('serializeUser', user);  //로그인에 성공 했을때 딱 1번
-    done(null, user.email);
-  });
-  
-  passport.deserializeUser(function(id, done) {
-    console.log('deserializeUser', id); // 그데이터를 기준으로 돌아다닐떄마다.
-    done(null, authData);
-  });
-
-  passport.use(new LocalStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'pwd'     
-    },
-    function(username, password, done) {
-      console.log('LocalStrategy', username, password);
-      if(username === authData.email){
-        console.log(1);
-        if(password === authData.password){
-          console.log(2);
-          return done(null, authData);
-        } else {
-          console.log(3);
-          return done(null, false, {
-            message: 'Incorrect password.'
-          });
-        }
-      } else {
-        console.log(4);
-        return done(null, false, {
-          message: 'Incorrect username.'
-        });
-      }
-    }
-  ));
-       
-  app.post('/auth/login_process',
-  passport.authenticate('local',{
-    successRedirect: '/',
-    failureRedirect: '/auth/login' 
-  }));
-
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
+app.get('*', function (request, response, next) {
+  fs.readdir('./data', function (error, filelist) {
     request.list = filelist;
     next();
   });
@@ -83,13 +33,13 @@ app.get('*', function(request, response, next){
 
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
-var authRouter = require('./routes/auth');
+var authRouter = require('./routes/auth')(passport);
 
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
 app.use('/auth', authRouter);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!');
 });
 
@@ -98,6 +48,6 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
--- routes/auth.js
+++ routes/auth.js
@@ -5,48 +5,45 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 
-var authData = {
-  email: 'amarans@gmail.com',
-  password: '111111',
-  nickname: 'amarans'
-}
 
-router.get('/login', function (request, response) {
-  var title = 'WEB - login';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/auth/login_process" method="post">
-      <p><input type="text" name="email" placeholder="email"></p>
-      <p><input type="password" name="pwd" placeholder="password"></p>
-      <p>
-        <input type="submit" value="login">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-});
 
-/*
-router.post('/login_process', function (request, response) {
-  var post = request.body;
-  var email = post.email;
-  var password = post.pwd;
-  if(email === authData.email && password === authData.password){
-    request.session.is_logined = true;
-    request.session.nickname = authData.nickname;
-    request.session.save(function(){
-      response.redirect(`/`);
-    });
-  } else {
-    response.send('Who?');
-  }
-});
-*/
 
-router.get('/logout', function (request, response) {
-  request.session.destroy(function(err){
-    response.redirect('/');
+module.exports = function (passport) {
+  router.get('/login', function (request, response) {
+    var fmsg = request.flash();
+    var feedback = '';
+    if (fmsg.error) {
+      feedback = fmsg.error[0];
+    }
+    var title = 'WEB - login';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+      <div style="color:red;">${feedback}</div>
+      <form action="/auth/login_process" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="pwd" placeholder="password"></p>
+        <p>
+          <input type="submit" value="login">
+        </p>
+      </form>
+    `, '');
+    response.send(html);
   });
-});
 
-module.exports = router;
+  router.post('/login_process',
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/auth/login',
+      failureFlash: true,
+      successFlash: true
+    }));
+
+  router.get('/logout', function (request, response) {
+    request.logout();
+    request.session.save(function () {
+      response.redirect('/');
+    });
+  });
+
+  return router;
+}
